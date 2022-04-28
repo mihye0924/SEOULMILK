aed77fc29c31616eda27a666351d165df5821d0e
chomihye <mihye0924@naver.com>
2022-04-28 21:32:34 +0900

    a

--- js/script.js
+++ js/script.js
@@ -1,46 +1,50 @@
-let pause = false;
-$(function(){      
+var pause = false;
+var sec = 0
+var click = false
+
+$(function(){       
     var content = $('.slide_content')
     var slide_wrap = $('.slide_wrap')
     var slideCount = content.length
     var slideWidth = $('.slide_content').width()
-    var slide_prograss_inner = $('.slide_prograss_inner')
-    var currentPosition = 0;  
-
+    var currentPosition = 0;   
     var totalSlideWidth = slideWidth*slideCount
-    slide_wrap.css({ width: totalSlideWidth }) 
 
 
-    var time = 1
-    var timer = setInterval(() => {  
-        if(!pause) {
-            $('.slide_more').trigger('click') 
+    // 슬라이드 네비게이션바 타이머
+    setInterval(() => {   
+        
+        if( sec >= 3 ) {
+            sec = 0
+            if(!pause) { 
+                $('.slide_more').trigger('click')  
+            }
+        } else {
+            if(!pause) {
+                sec = Number(sec.toFixed(2)) + 0.01 
+            }
         }
-        console.log(time) 
-        time++
-    }, 3000);
-
-
-
+ 
+    }, 10);
+ 
     // 마우스 올렸을때
     $('.slide_content, .slide_more').on('mouseover', function(){  
-        $(slide_prograss_inner).addClass('pause')
-        clearInterval(timer)
-        pause = true
+        $('.slide_prograss_inner').addClass('pause')
+        pause = true 
     }) 
     $('.slide_content, .slide_more').on('mouseout', function(){ 
-        $(slide_prograss_inner).removeClass('pause')
-        timer = setInterval()
+        $('.slide_prograss_inner').removeClass('pause')
         pause = false
-    })
-  
+    }) 
 
-    for(i=0;i<content.length;i++){ 
-            $(slide_prograss_inner).addClass('animate')
-    } 
+    // 애니메이션 - 텍스트, 이미지
+    $('.slide_prograss_inner').addClass('animate')   
+    $('.textBox .title').css({ bottom: '-125px'}).animate({ bottom: 0 })
+    $('.textBox .subTitle').css({ bottom: '-80px'}).animate({ bottom: 0 })
 
     // 이전 클릭
-    $('.slide_more').on('click',function(){
+    $('.slide_more').on('click',function(){ 
+        slide_wrap.css({ width: totalSlideWidth })     
         content.css({ left : -currentPosition , opacity: 0.5 }).animate({ opacity : 1}) 
         $('.slide_content:first-child').appendTo(slide_wrap) 
         
@@ -50,12 +54,35 @@ $(function(){
             var prev_count = document.querySelector('.prev_count').innerHTML 
             prev_count++;
             document.querySelector('.prev_count').textContent = '0'+prev_count
-    
-        
+     
             if(prev_count > 3 ){
                 document.querySelector('.prev_count').textContent = '0'+1
-            }    
-        }  
+            }  
+        } 
+
+        $('.textBox .title').css({ bottom: '-125px'}).animate({ bottom: 0 })
+        $('.textBox .subTitle').css({ bottom: '-80px'}).animate({ bottom: 0 })   
     })
+
+
+    //fresh milk 텍스트 애니메이션
+    var fresh = document.querySelector('.title01 span:first-of-type').innerHTML
+    console.log(fresh)
+
+    var timer = 0;
+    setInterval(() => { 
+        timer+=1;
+
+        if(timer % 2 == 0){
+            $('h1.title01 span:first-of-type').css({top:'-210px'}).animate({top:'0'},500)
+            document.querySelector('.title01 span:first-of-type').textContent = 'FRESH'
+        }else{
+            $('h1.title01 span:first-of-type').css({top:'-210px'}).animate({top:'0'},500)
+            document.querySelector('.title01 span:first-of-type').textContent = 'SPECIAL'
+        }
+        console.log(timer)
+      
+    }, 4000);
  
 }) 
+ 
